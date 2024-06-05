e9489be67f254114cc9c8e085514c91435ae6011
itzKv <kenkenku6@gmail.com>
2024-06-05 23:03:29 +0700

    Body Complete

--- lectuta-app/app/Http/Controllers/NotesController.php
+++ lectuta-app/app/Http/Controllers/NotesController.php
@@ -3,39 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 use File;
 
 class NotesController extends Controller
 {
     public function index() {
-        $createdAt = "2024-04-22"; // dummy
-        $filename = null;
-        $notes = [];
-        if ($handle = opendir(public_path('template/assets/audio'))) {
-
-            while (false !== ($entry = readdir($handle))) {
-                if ($entry != "." && $entry != "..") {
-                    $filename = $entry; // NAME OF THE FILE
-                    break;
-                }
-            }
-            closedir($handle);
-        }
-
-        $dummyFilePath = public_path('template/assets/notes/dummy.txt');
-        $dummyFileContents = file($dummyFilePath);
-
-        if ($handle = opendir(public_path('template/assets/notes'))) {
-
-            while (false !== ($entry = readdir($handle))) {
-                if ($entry != "." && $entry != "..") {
-                    $notes = $dummyFileContents;
-                    break;
-                }
-            }
-            closedir($handle);
-        }
-
+        $createdAt = "2024-04-22"; // dari BE
+        $filename = Session::get('filename'); // harusnya panggil dari database
+        $notes = "
+            <div class=\"notes-sub-heading mt-4 ms-4 me-4\">
+                    <h3 style=\"color: #000000;\">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+                </div>
+                <div class=\"notes-content ms-4 me-4\">
+                    <ul>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum unde quam, fugiat, cumque, explicabo a modi mollitia ipsum sed repellendus qui adipisci laboriosam reiciendis voluptas est veniam atque cupiditate itaque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, reiciendis hic fuga dolores expedita voluptatum saepe ipsum consectetur dicta voluptas incidunt dolorem sequi quia esse pariatur dolorum totam doloribus dignissimos.</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat aspernatur reprehenderit suscipit non, itaque nihil, dolorum aperiam voluptate odio laboriosam quia nam deserunt, cupiditate ut at commodi maiores ducimus eaque?</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ipsam cupiditate sunt. Qui aspernatur sed laboriosam consequuntur quasi provident veritatis deleniti placeat, excepturi dicta cupiditate itaque, non officiis fugit doloremque.</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus explicabo, eum autem amet cumque similique. Minus illo nisi, esse cupiditate dolorum magnam, explicabo vel totam vitae ut saepe officia numquam!</li>
+                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit reiciendis, odit pariatur fuga corporis qui quasi a, distinctio tenetur est, sapiente quidem doloremque sequi maxime vitae nulla error odio earum! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum tempore porro maiores repellendus. At repudiandae officia a enim aliquid atque eius, explicabo aspernatur libero corporis ea! Iure doloribus consequatur ex. Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde aliquid magnam iste ipsum saepe blanditiis, facere excepturi ut. Impedit numquam sed necessitatibus maiores. Voluptas quis vel id facilis dignissimos omnis.</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nihil commodi magnam deleniti. Mollitia harum, ipsum sit cum iure fugit maiores delectus ab praesentium eligendi! Doloribus tempore tempora pariatur voluptas!</li>
+                    </ul>
+            </div>
+        "; // Dari BE
         return view('notes.generate', compact('filename', 'createdAt', 'notes'));
     }
 }
--- lectuta-app/resources/views/notes/generate.blade.php
+++ lectuta-app/resources/views/notes/generate.blade.php
@@ -43,7 +43,7 @@
     <div class="min-height-500 border-radius-xl mt-4" style="background-color: #B8B8B8;">
         <div class="row">
             <div class="col-lg-10">
-                <h1 class="mt-4 mb-4 ms-4" style="color: #000000; size: 28px;">Title of the documents</h1>
+                <h1 class="mt-4 mb-4 ms-4" style="color: #000000; size: 28px;">{!! $filename !!}</h1>
             </div>
             <div class="col-lg-2">
                 <div class="float-lg-end pe-4">
@@ -55,32 +55,10 @@
 
         <hr>
     
-        <div class="col-lg-12 ms-2" style="max-height: 380px; overflow-y: auto;">
-            <div class="notes-sub-heading mt-4 ms-4 me-4">
-                <h3 style="color: #000000;">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
-            </div>
-            <div class="notes-content ms-4 me-4">
-                <ul>
-                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum unde quam, fugiat, cumque, explicabo a modi mollitia ipsum sed repellendus qui adipisci laboriosam reiciendis voluptas est veniam atque cupiditate itaque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, reiciendis hic fuga dolores expedita voluptatum saepe ipsum consectetur dicta voluptas incidunt dolorem sequi quia esse pariatur dolorum totam doloribus dignissimos.</li>
-                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat aspernatur reprehenderit suscipit non, itaque nihil, dolorum aperiam voluptate odio laboriosam quia nam deserunt, cupiditate ut at commodi maiores ducimus eaque?</li>
-                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ipsam cupiditate sunt. Qui aspernatur sed laboriosam consequuntur quasi provident veritatis deleniti placeat, excepturi dicta cupiditate itaque, non officiis fugit doloremque.</li>
-                    <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus explicabo, eum autem amet cumque similique. Minus illo nisi, esse cupiditate dolorum magnam, explicabo vel totam vitae ut saepe officia numquam!</li>
-                    <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit reiciendis, odit pariatur fuga corporis qui quasi a, distinctio tenetur est, sapiente quidem doloremque sequi maxime vitae nulla error odio earum! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum tempore porro maiores repellendus. At repudiandae officia a enim aliquid atque eius, explicabo aspernatur libero corporis ea! Iure doloribus consequatur ex. Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde aliquid magnam iste ipsum saepe blanditiis, facere excepturi ut. Impedit numquam sed necessitatibus maiores. Voluptas quis vel id facilis dignissimos omnis.</li>
-                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nihil commodi magnam deleniti. Mollitia harum, ipsum sit cum iure fugit maiores delectus ab praesentium eligendi! Doloribus tempore tempora pariatur voluptas!</li>
-                </ul>
-            </div>
-
-            <div class="notes-sub-heading mt-4 ms-4 me-4">
-                <h3 style="color: #000000;">Lorem ipsum amet consectetur elit.</h3>
-            </div>
-            <div class="notes-content ms-4 me-4">
-                <ul>
-                    <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus explicabo, eum autem amet cumque similique. Minus illo nisi, esse cupiditate dolorum magnam, explicabo vel totam vitae ut saepe officia numquam!</li>
-                    <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit reiciendis, odit pariatur fuga corporis qui quasi a, distinctio tenetur est, sapiente quidem doloremque sequi maxime vitae nulla error odio earum! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum tempore porro maiores repellendus. At repudiandae officia a enim aliquid atque eius, explicabo aspernatur libero corporis ea! Iure doloribus consequatur ex. Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde aliquid magnam iste ipsum saepe blanditiis, facere excepturi ut. Impedit numquam sed necessitatibus maiores. Voluptas quis vel id facilis dignissimos omnis.</li>
-                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nihil commodi magnam deleniti. Mollitia harum, ipsum sit cum iure fugit maiores delectus ab praesentium eligendi! Doloribus tempore tempora pariatur voluptas!</li>
-                </ul>
-            </div>
+        <div class="col-lg-12 ms-2" id="notes-body" style="max-height: 380px; overflow-y: auto;">
+            {!! $notes !!}
         </div>
+        
     </div>
 </div>
 
@@ -88,18 +66,13 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
     $(document).ready(function() {
-        e.preventDefault();
-        $.ajax({
-            url: '/audio/upload',
-            type: 'GET',
-            success: function(response) {
-                $('#audioFileName').text(response.filename);
-            },
-            error: function(xhr, status, error) {
-                console.error(error);
-            }
-        });
-    })  
+        // Check if $filename exists
+        @if(isset($filename))
+            console.log("Filename exists:", {!! json_encode($filename) !!});
+        @else
+            console.log("Filename does not exist.");
+        @endif
+    });
 </script>
 
 @endsection 
